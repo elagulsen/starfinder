69168d8e257f823280a5024884228debf7003409
ladyt <egulsen@stuy.edu>
2020-07-24 02:19:10 -0400

    added star list functionality!

---
+++ js/astro.coords.js
@@ -0,0 +1,82 @@
+// compute altitude and azimuth
+function compute(ra, dec, lat, lon) {
+	const now = new Date();
+	now.setHours(0, 0);//-(now.getTimezoneOffset() / 60), 0); // => utc 0
+
+  return coord_to_horizon(now, ra, dec, lat, lon);
+
+}
+
+// compute horizon coordinates from ra, dec, lat, lon, and utc
+// ra, dec, lat, lon in  degrees
+// utc is a Date object
+// results returned in hrz_altitude, hrz_azimuth
+function coord_to_horizon( utc, ra, dec, lat, lon ) {
+    // compute hour angle in degrees
+    let ha = mean_sidereal_time( utc, lon ) - ra;
+    if (ha < 0) ha = ha + 360;
+
+    // convert degrees to radians
+    ha  = ha*Math.PI/180
+    dec = dec*Math.PI/180
+    lat = lat*Math.PI/180
+
+    // compute altitude in radians
+    const sin_alt = Math.sin(dec)*Math.sin(lat) + Math.cos(dec)*Math.cos(lat)*Math.cos(ha);
+    const alt = Math.asin(sin_alt);
+
+    // compute azimuth in radians
+    // divide by zero error at poles or if alt = 90 deg
+    const cos_az = (Math.sin(dec) - Math.sin(alt)*Math.sin(lat))/(Math.cos(alt)*Math.cos(lat));
+    const az  = Math.acos(cos_az);
+
+    // convert radians to degrees
+    const hrz_altitude = alt*180/Math.PI;
+    const hrz_azimuth  = az*180/Math.PI;
+
+    // choose hemisphere
+    if (Math.sin(ha) > 0) hrz_azimuth = 360 - hrz_azimuth;
+
+		return [hrz_altitude, hrz_azimuth];
+}
+
+// Compute the Mean Sidereal Time in units of degrees.
+// Use lon := 0 to get the Greenwich MST.
+// East longitudes are positive; West longitudes are negative
+// returns: time in degrees
+function mean_sidereal_time(now, lon) {
+    var year   = now.getUTCFullYear();
+    var month  = now.getUTCMonth() + 1;
+    var day    = now.getUTCDate();
+    var hour   = now.getUTCHours();
+    var minute = now.getUTCMinutes();
+    var second = now.getUTCSeconds();
+
+    if ((month == 1)||(month == 2))
+    {
+        year  = year - 1;
+        month = month + 12;
+    }
+
+    var a = Math.floor(year/100);
+    var b = 2 - a + Math.floor(a/4);
+    var c = Math.floor(365.25*year);
+    var d = Math.floor(30.6001*(month + 1));
+
+    // days since J2000.0
+    var jd = b + c + d - 730550.5 + day + (hour + minute/60.0 + second/3600.0)/24.0;
+
+    // julian centuries since J2000.0
+    var jt = jd/36525.0;
+
+    // the mean sidereal time in degrees
+    var mst = 280.46061837 + 360.98564736629*jd + 0.000387933*jt*jt - jt*jt*jt/38710000 + lon;
+
+    // in degrees modulo 360.0
+    if (mst > 0.0)
+        while (mst > 360.0) mst = mst - 360.0;
+    else
+        while (mst < 0.0)   mst = mst + 360.0;
+
+    return mst;
+}
--- js/script.js
+++ js/script.js
@@ -4,49 +4,81 @@ const result = document.getElementById('result');
 
 const onZipButtonPress = function() {
   const value = zipInput.value;
-  if (zipcodes[value]) result.innerHTML = displayMessage(zipcodes[value]);
+  console.log(compute(29.09, 0.139, zipcodes[value][1][0], zipcodes[value][1][1]));
+  if (zipcodes[value]) result.innerHTML = displayBortle(value);
   else result.innerHTML = "Sorry, we don't have this zipcode in our system. It's possible you typed it in wrong, or there is no SQM data for this zip code.";
 }
 
-function displayMessage(sqm) {
+const calculateNelm = function(sqm) {
+  if (sqm > 20) return ((sqm-8.89)/2)+1;
+  if (sqm > 17) return (sqm-8.89)/2;
+  else return ((sqm-8.89)/2)-1;
+}
+
+const starCount = function(value) {
+  let starNum = 0;
+  let nelm = calculateNelm(zipcodes[value][0]);
+  let stars = [];
+  for (var key in hygfull) if (hygfull[key][2] <= nelm && compute(hygfull[key][0], hygfull[key][1], zipcodes[value][1][0], zipcodes[value][1][1])[0] > 0) {
+      starNum++;
+      if (hygfull[key][3] != "" && hygfull[key][3] != " ") stars.push(hygfull[key]);
+    }
+  return [starNum - starNum % 10, stars];
+}
+
+function displayBortle(value) {
+  let sqm = zipcodes[value][0];
+  let msg = "";
   if (sqm < 17.8) {
-    return "<img src=\"img/bortle_scale.jpeg\"><br><br>Your zip code falls into class 9 of the Bortle Scale: an <b>inner city sky</b>!\
+    msg = "<img src=\"img/bortle_scale.jpeg\"><br><br>Your zip code falls into class 9 of the Bortle Scale: an <b>inner city sky</b>!\
             <br><br>This means that you can see very few stars in the sky due to heavy light pollution in your area.\
             <br>The sky is brightly lit and has a strong glow (\"skyglow\"), caused by the use of artificial light at night,\
             which causes all but a select few stars to be invisible..\
             <br>"
   }
   else if (sqm < 18.38) {
-    return "<img src=\"img/bortle_scale.jpeg\"><br><br>Your zip code falls into class 8 of the Bortle Scale: a <b>city sky</b>!\
+    msg = "<img src=\"img/bortle_scale.jpeg\"><br><br>Your zip code falls into class 8 of the Bortle Scale: a <b>city sky</b>!\
             <br><br>Although not quite as bad as an inner city sky, this means that you can see only a few stars due to light pollution in your area.\
             The sky is brightly lit and has a gray or orange glow (\"skyglow\"), caused by the use of artificial light at night,\
             which causes most stars to be invisible.\
             <br>"
   }
   else if (sqm < 18.95) {
-    return "<img src=\"img/bortle_scale.jpeg\"><br>Your zip code falls into class 7 of the Bortle Scale: a <b>suburban-urban sky</b>!"
+    msg = "<img src=\"img/bortle_scale.jpeg\"><br>Your zip code falls into class 7 of the Bortle Scale: a <b>suburban-urban sky</b>!"
   }
   else if (sqm < 19.5) {
-    return "<img src=\"img/bortle_scale.jpeg\"><br>Your zip code falls into class 6 of the Bortle Scale: a <b>bright suburban transition sky</b>!"
+    msg = "<img src=\"img/bortle_scale.jpeg\"><br>Your zip code falls into class 6 of the Bortle Scale: a <b>bright suburban transition sky</b>!"
   }
   else if (sqm < 20.49) {
-    return "<img src=\"img/bortle_scale.jpeg\"><br>Your zip code falls into class 5 of the Bortle Scale: a <b>suburban sky</b>!"
+    msg = "<img src=\"img/bortle_scale.jpeg\"><br>Your zip code falls into class 5 of the Bortle Scale: a <b>suburban sky</b>!"
   }
   else if (sqm < 21.69) {
-    return "<img src=\"img/bortle_scale.jpeg\"><br>Your zip code falls into class 4 of the Bortle Scale: a <b>rural-suburban transition sky</b>!"
+    msg = "<img src=\"img/bortle_scale.jpeg\"><br>Your zip code falls into class 4 of the Bortle Scale: a <b>rural-suburban transition sky</b>!"
   }
   else if (sqm < 21.89) {
-    return "<img src=\"img/bortle_scale.jpeg\"><br>Your zip code falls into class 3 of the Bortle Scale: a <b>rural sky</b>!"
+    msg = "<img src=\"img/bortle_scale.jpeg\"><br>Your zip code falls into class 3 of the Bortle Scale: a <b>rural sky</b>!"
   }
   else if (sqm < 21.99) {
-    return "<img src=\"img/bortle_scale.jpeg\"><br>Your zip code falls into class 2 of the Bortle Scale: a <b>truly dark sky</b>!\
+    msg = "<img src=\"img/bortle_scale.jpeg\"><br>Your zip code falls into class 2 of the Bortle Scale: a <b>truly dark sky</b>!\
             <br>Wow!"
   }
   else {
-    return "<img src=\"img/bortle_scale.jpeg\"><br>Your zip code falls into class 1 of the Bortle Scale: an <b>excellent dark sky site</b>!\
+    msg = "<img src=\"img/bortle_scale.jpeg\"><br>Your zip code falls into class 1 of the Bortle Scale: an <b>excellent dark sky site</b>!\
             <br>You are very lucky to be seeing the beauty of the true night sky, as this is extremely rare.\
             <br>In an excellent dark sky site, you will be able to see more than 4,500 stars, as well as the Milky Way."
   }
+  let st = starCount(value);
+
+  st[1].sort(function(val1, val2) {return parseFloat(val1[2]) - parseFloat(val2[2])});
+
+  msg += "<br>Based on the sky quality meter readings in your area, you might be able to see " + st[0] + " stars in the sky!<br>";
+  msg += "<br>Here are some stars you might be able to see, in order of decreasing brightness: <br>";
+
+  for (star in st[1]) {
+    msg += "<b>" + st[1][star][3] + "</b><br>";
+  }
+
+  return msg;
 }
 
 zipButton.addEventListener('click', onZipButtonPress);
